477d427231d6fa69678e2de49168ee08dfec304b
alessiavozzo <a.vozzo95@gmail.com>
2024-06-03 11:50:45 +0200

    add endpoint for single project

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -9,10 +9,27 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with('type', 'technologies')->paginate(3);
+        $projects = Project::with('type', 'technologies')->paginate(6);
         return response()->json([
             'success' => true,
             'projects' => $projects
         ]);
+    }
+
+    public function show($id)
+    {
+        $project = Project::with('type', 'technologies')->where('id', $id)->first();
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'response' => $project
+            ]);
+        } else {
+            /* 404 */
+            return response()->json([
+                'success' => false,
+                'response' => 'sorry nothing found'
+            ]);
+        }
     }
 }
